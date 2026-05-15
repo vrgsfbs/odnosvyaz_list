60c31f797ff48d3f5e0a8c414ade0c313124e1e1
vrgsfbs <vrgs@vrgsfbs.ru>
2026-05-15 05:46:24 +0300

    отично, но без методов многих и свойств и без задания и без инуберабле итд

---
+++ odnosvyaz_list/Program.cs
@@ -0,0 +1,102 @@
+﻿﻿﻿using System.ComponentModel;
+using System.Net.WebSockets;
+
+namespace odnosvyaz_list
+{
+    internal class Program
+    {
+        public class Node<T>
+        {
+            public Node(T data)
+            {
+                Data = data;
+            }
+            public T Data { get; set; }
+            public Node<T>? Next { get; set; }
+        }
+        public class LinkedListM<T>
+        {
+          Node<T>? head;
+          Node<T>? tail;
+          int count = 0;
+          public void Add(T data)
+            {
+                Node<T> node = new Node<T>(data);
+                if (head == null)
+                    head = node;
+                else
+                    tail!.Next = node;
+                tail = node;
+        
+                count++;
+            }
+             public bool Remove(T data)
+    {
+        Node<T>? current = head;
+        Node<T>? previous = null;
+ 
+        while (current != null && current.Data != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                if (previous != null)
+                {
+                    previous.Next = current.Next;
+ 
+
+                    if (current.Next == null)
+                        tail = previous;
+                }
+                else
+                {
+
+                    head = head?.Next;
+ 
+                    if (head == null)
+                        tail = null;
+                }
+                count--;
+                return true;
+            }
+ 
+            previous = current;
+            current = current.Next;
+        }
+        return false;
+    }
+ 
+    public int Count { get { return count; } }
+    public bool IsEmpty { get { return count == 0; } }
+    public void Clear()
+    {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+    public bool Contains(T data)
+    {
+        Node<T>? current = head;
+        while (current != null && current.Data !=null)
+        {
+            if (current.Data.Equals(data)) return true;
+            current = current.Next;
+        }
+        return false;
+    }
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
